016f9a1de4e8c74e6e1cdc9e3df77e92736f00bc
Dmitrii Forofontov <forofontov.dev@gmail.com>
2022-04-17 13:13:26 +0200

    fix: random command

--- src/handlers/commands/handle-rand.ts
+++ src/handlers/commands/handle-rand.ts
@@ -5,7 +5,7 @@ export const isRand = (ctx: NarrowedContext<Context, MountMap['text']>): boolean
     const [command, ...args] = ctx.update.message.text.split(' ').filter(Boolean);
 
     if (command === '/rand' && args.length >= 2) {
-        if (args.length === 2 && args.every((n) => +n !== NaN)) {
+        if (args.length === 2 && args.every((arg) => !Number.isNaN(+arg))) {
             handleRandNumber(ctx);
         } else {
             handleRandProp(ctx);
@@ -18,7 +18,6 @@ export const isRand = (ctx: NarrowedContext<Context, MountMap['text']>): boolean
 const handleRandNumber = (ctx: NarrowedContext<Context, MountMap['text']>) => {
     let { 1: min, 2: max } = ctx.update.message.text.split(' ');
     if (min > max) [min, max] = [max, min];
-    console.log(Math.floor(Math.random() * (Math.floor(+max) - Math.floor(+min) + 1)));
     const rand = Math.floor(Math.random() * (Math.floor(+max) - Math.floor(+min) + 1)) + Math.floor(+min);
 
     ctx.reply(`${rand}`);
@@ -28,5 +27,7 @@ const handleRandProp = (ctx: NarrowedContext<Context, MountMap['text']>) => {
     const [, ...args] = ctx.update.message.text.split(' ').filter(Boolean);
 
     const randIndex = Math.floor(Math.random() * args.length);
+    console.log(randIndex);
+
     ctx.reply(`${args[randIndex]}`);
 };
