5b35a1345c948daa87de3c0d1ce5f7a0a3a971dd
Dmitrii Forofontov <forofontov.dev@gmail.com>
2022-04-17 17:53:52 +0200

    feat: add summon cooldown

--- src/handlers/commands/handle-summon.ts
+++ src/handlers/commands/handle-summon.ts
@@ -3,10 +3,14 @@ import { Context, NarrowedContext } from 'telegraf';
 import { Message } from 'telegraf/typings/core/types/typegram';
 import { MountMap } from 'telegraf/typings/telegram-types';
 
+let isCooldown = false;
+
 export const handleSummon = (ctx: NarrowedContext<Context, MountMap['text']>) => {
     const [, ...args] = ctx.update.message.text.split(' ').filter(Boolean);
-    
-    if (!args.length || !args.every((arg) => arg[0] === '@')) return;
+
+    if (!args.length || !args.every((arg) => arg[0] === '@') || isCooldown) return;
+
+    setCooldown();
 
     const CATEGORY = 'summon-jutsu';
     const GIPHY_API_KEY = <string>process.env.GIPHY_API_KEY;
@@ -51,4 +55,13 @@ const deleteSummonMessages = (messages: Message.TextMessage[], ctx: Context) => 
             ctx.telegram.deleteMessage(chatId, messages[i].message_id);
         }, DELETE_TIMEOUT);
     }
+};
+
+const setCooldown = () => {
+    isCooldown = true;
+    const COOLDOWN_TIMEOUT = 60_000;
+
+    setTimeout(() => {
+        isCooldown = false;
+    }, COOLDOWN_TIMEOUT);
 };
